4196d92a5543054cc792895d75476014bfdd79a1
sabilaindah5-stack <sabilaindah5@gmail.com>
2026-01-07 11:13:17 +0700

    Add files via upload

---
+++ # Fungsi untuk mencetak pola segitiga bi 2026.py
@@ -0,0 +1,8 @@
+# Fungsi untuk mencetak pola segitiga bintang
+def cetak_pola(baris):
+    for i in range(1, baris + 1):
+        print("*" * i)
+
+# Program utama
+jumlah_baris = int(input("Masukkan jumlah baris: "))
+cetak_pola(jumlah_baris)
