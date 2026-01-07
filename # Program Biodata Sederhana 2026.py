f29ab8c8f965c1e8798c1e2633c574179d53ddbd
sabilaindah5-stack <sabilaindah5@gmail.com>
2026-01-07 11:12:05 +0700

    Add files via upload

---
+++ # Program Biodata Sederhana 2026.py
@@ -0,0 +1,15 @@
+# Program Biodata Sederhana
+
+# Input data dari pengguna
+nama = input("Masukkan nama       : ")
+usia = input("Masukkan usia       : ")
+alamat = input("Masukkan alamat     : ")
+hobi = input("Masukkan hobi        : ")
+
+# Menampilkan biodata
+print("\n===== BIODATA =====")
+print("Nama   :", nama)
+print("Usia   :", usia, "tahun")
+print("Alamat :", alamat)
+print("Hobi   :", hobi)
+print("===================")
